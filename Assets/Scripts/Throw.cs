ebd832d4e76503f8aaf140841ed11550fa387391
VR2023-02\Nishino <draneko0927@gmail.com>
2024-02-13 14:42:52 +0900

    斜方投射まで実装

--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -4,41 +4,109 @@ using UnityEngine;
 
 public class Throw : MonoBehaviour
 {
-    public GameObject target; // Change this line
-    public float firingAngle = 45.0f;
-    public float gravity = 9.8f;
+    /// <summary>
+    /// 射出するオブジェクト
+    /// </summary>
+    [SerializeField, Tooltip("射出するオブジェクトをここに割り当てる")]
+    private GameObject ThrowingObject;
 
-    public void Start()
+    /// <summary>
+    /// 標的のオブジェクト
+    /// </summary>
+    [SerializeField, Tooltip("標的のオブジェクトをここに割り当てる")]
+    private GameObject TargetObject;
+
+    /// <summary>
+    /// 射出角度
+    /// </summary>
+    [SerializeField, Range(0F, 90F), Tooltip("射出する角度")]
+    private float ThrowingAngle;
+
+    private void Start()
     {
-        StartCoroutine(SimulateProjectile());
+        Collider collider = GetComponent<Collider>();
+        if (collider != null)
+        {
+            // 干渉しないようにisTriggerをつける
+            collider.isTrigger = true;
+        }
+
+        // 1秒ごとにボールを射出するコルーチンを開始
+        StartCoroutine(ThrowingBallRoutine()); // 追加箇所
     }
 
-    IEnumerator SimulateProjectile()
+    private void Update()
     {
-        // 初期位置を取得
-        Vector3 startPos = transform.position;
+        
+    }
 
-        // 目標までの距離を計算
-        float target_Distance = Vector3.Distance(startPos, target.transform.position); // Change this line
+    /// <summary>
+    /// ボールを射出する
+    /// </summary>
+    private void ThrowingBall()
+    {
 
-        // 初速度を計算
-        float projectile_Velocity = target_Distance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / gravity);
-
-        // X軸とY軸の速度を計算
-        float Vx = Mathf.Sqrt(projectile_Velocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
-        float Vy = Mathf.Sqrt(projectile_Velocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
-
-        // 飛行時間を計算
-        float flightDuration = target_Distance / Vx;
-
-        // 投射体を飛ばす
-        float elapse_time = 0;
-
-        while (elapse_time < flightDuration)
+        if (ThrowingObject != null && TargetObject != null)
         {
-            transform.position = new Vector3(startPos.x + Vx * elapse_time, startPos.y + Vy * elapse_time - (0.5f * gravity * Mathf.Pow(elapse_time, 2)), startPos.z);
-            elapse_time += Time.deltaTime;
-            yield return null;
+            // Ballオブジェクトの生成
+            GameObject ball = Instantiate(ThrowingObject, this.transform.position, Quaternion.identity);
+
+            // 標的の座標
+            Vector3 targetPosition = TargetObject.transform.position;
+
+            // 射出角度
+            float angle = ThrowingAngle;
+
+            // 射出速度を算出
+            Vector3 velocity = CalculateVelocity(this.transform.position, targetPosition, angle);
+
+            // 射出
+            Rigidbody rid = ball.GetComponent<Rigidbody>();
+            rid.AddForce(velocity * rid.mass, ForceMode.Impulse);
+        }
+        else
+        {
+            throw new System.Exception("射出するオブジェクトまたは標的のオブジェクトが未設定です。");
+        }
+    }
+
+    /// <summary>
+    /// 標的に命中する射出速度の計算
+    /// </summary>
+    /// <param name="pointA">射出開始座標</param>
+    /// <param name="pointB">標的の座標</param>
+    /// <returns>射出速度</returns>
+    private Vector3 CalculateVelocity(Vector3 pointA, Vector3 pointB, float angle)
+    {
+        // 射出角をラジアンに変換
+        float rad = angle * Mathf.PI / 180;
+
+        // 水平方向の距離x
+        float x = Vector2.Distance(new Vector2(pointA.x, pointA.z), new Vector2(pointB.x, pointB.z));
+
+        // 垂直方向の距離y
+        float y = pointA.y - pointB.y;
+
+        // 斜方投射の公式を初速度について解く
+        float speed = Mathf.Sqrt(-Physics.gravity.y * Mathf.Pow(x, 2) / (2 * Mathf.Pow(Mathf.Cos(rad), 2) * (x * Mathf.Tan(rad) + y)));
+
+        if (float.IsNaN(speed))
+        {
+            // 条件を満たす初速を算出できなければVector3.zeroを返す
+            return Vector3.zero;
+        }
+        else
+        {
+            return (new Vector3(pointB.x - pointA.x, x * Mathf.Tan(rad), pointB.z - pointA.z).normalized * speed);
+        }
+    }
+    private IEnumerator ThrowingBallRoutine() // 追加箇所
+    {
+        while (true)
+        {
+            ThrowingBall();
+            yield return new WaitForSeconds(1);
         }
     }
 }
+
